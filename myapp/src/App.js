5e43cc56a11b1ee2094d3435296d540787313113
Mati <matiaskelman@gmail.com>
2025-09-11 17:43:01 -0300

    Footer added

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Switch,Route } from "react-router-dom"
 import Header from './Components/Header/Header.js';
+import Footer from './Components/Footer/Footer.js';
 import Home from './screens/home/Home.js';
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
       <Header />
       <Switch>
         <Route path='/' component={Home} exact={true} />
+        <Route component={() => <div>404 NOT FOUND</div>} />
       </Switch>
+      <Footer/>
     </React.Fragment>
 
   );
---
+++ myapp/src/Components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+        <div>
+            <ul>
+                <li>Matias Kelman</li>
+                <li>Lautaro Poggi</li>
+                <li>Lautaro Porta</li>
+            </ul>
+        </div>
+    )
+  }
+}
+export default Footer;
