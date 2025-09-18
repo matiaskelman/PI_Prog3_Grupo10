4966d1c8700a7e3258e6fcd4e436cc471d666a44
Mati <matiaskelman@gmail.com>
2025-09-18 13:14:06 -0300

    Pagina detalle ok, sin css

--- myapp/src/screens/Detalle/Detalle.js
+++ myapp/src/screens/Detalle/Detalle.js
@@ -13,18 +13,28 @@ export class Detalle extends Component {
     };
   }
   componentDidMount() {
-
-    fetch( `https://api.themoviedb.org/3/movie/${this.props.id}?api_key=f9fed29318027d1571e2d4e385ce272d&language=en-US`)
+let id = this.props.match.params.id
+    fetch( `https://api.themoviedb.org/3/movie/${id}?api_key=f9fed29318027d1571e2d4e385ce272d&language=es-ES&page=1`)
       .then(response => response.json())
       .then(data => {
-        this.setState({ detalle: data.results, cargando: false })
+        this.setState({ detalle: data, cargando: false })
         console.log(data)
       })
       .catch(() => this.setState({ detalle: [] }));
   }
   render() {
+    const item = this.state.detalle;
     return (
-      <div>{this.detalle}</div>
+      
+    <article className="ficha">
+      <h1>{item.title}</h1>
+      <h2>{item.vote_average}</h2>
+      <h2>{item.release_date}</h2>
+      <p>{item.overview}</p>
+      <img src={`https://image.tmdb.org/t/p/w342${item.poster_path}`} alt={item.title} />
+      <a href= {`${item.homepage}`} ><h3>Pagina web</h3></a>
+      
+      </article>
     )
   }
 }
