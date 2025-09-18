69ac2b663f2a195df4c052e9b69535d99787b6ce
“Lautiporta” <lportacampos@udesa.edu.ar>
2025-09-18 20:01:54 -0300

    cargarmas en screen peliculas y seriesView finalizado

--- myapp/src/screens/Peliculas/Peliculas.js
+++ myapp/src/screens/Peliculas/Peliculas.js
@@ -1,31 +1,55 @@
 import React, { Component } from 'react'
 import Movies from '../../Components/Movies/movies';
 export class Peliculas extends Component {
-   constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       data: [],
       cargando: true,
+      pag: 1
     };
+
   }
-    componentDidMount() {
-  
+  componentDidMount() {
+
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=f9fed29318027d1571e2d4e385ce272d&language=es-ES&page=1')
       .then(response => response.json())
       .then(data => {
-        this.setState({ data: data.results, cargando: false })
-        console.log(data)
+        this.setState({
+          data: data.results,
+          cargando: false,
+          pag: this.state.pag + 1
+        })
       })
-      .catch(() => this.setState({ data: [] }));
+      .catch(() => this.setState({ data: [], cargando: false }));
   }
+cargarMas() {
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f9fed29318027d1571e2d4e385ce272d&language=es-ES&page=${this.state.pag}`)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        data: this.state.data.concat(data.results),
+        pag: this.state.pag + 1
+      });
+    })
+    .catch(() => {});
+}
+
+  
+
+
+
   render() {
     return (
       <div>
-                <Movies
-              titulo="Popular movies this week"
-              videos={this.state.data}
-              toAll="/movies?mode=popular"
-            />
+        <Movies
+          titulo="Popular movies this week"
+          videos={this.state.data}
+          toAll="/movies?mode=popular"
+        />
+         {this.state.cargando ? null : (
+          <button onClick={() => this.cargarMas()}>Cargar más</button>
+         )}
       </div>
 
     )
--- myapp/src/screens/Series/SeriesView.js
+++ myapp/src/screens/Series/SeriesView.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react'
 import Series from '../../Components/Series/series';
+
+
 export class SeriesView extends Component {
    constructor(props) {
     super(props);
     this.state = {
       series: [],
       cargando: true,
+      pag: 1
     };
+    
   }
     componentDidMount() {
   
     fetch('https://api.themoviedb.org/3/tv/popular?api_key=f9fed29318027d1571e2d4e385ce272d&language=es-ES&page=1')
       .then(response => response.json())
       .then(data => {
-        this.setState({ series: data.results, cargando: false })
-        console.log(data)
+        this.setState(
+          { series: data.results || [], 
+          cargando: false,
+          pag: this.state.pag + 1
+        });
+        
       })
-      .catch(() => this.setState({ series: [] }));
+      .catch(() => this.setState({ series: [],cargando:false }));
   }
+
+    cargarMas() {
+    fetch(`https://api.themoviedb.org/3/tv/popular?api_key=f9fed29318027d1571e2d4e385ce272d&language=es-ES&page=${this.state.pag}`)
+      .then(response => response.json())
+      .then(data => {
+      this.setState({
+          series: this.state.series.concat(data.results),
+          pag: this.state.pag + 1
+        });
+      })
+       .catch((error) => console.log("Error Fetch", error));
+    }
   render() {
     return (
       <div>
         <Series data = {this.state.series}/>
+        {this.state.cargando ? null : (
+          <button onClick={() => this.cargarMas()} className="cargarMas">
+            Cargar más
+          </button> )}
+        
       </div>
 
     )
